08b79713bf3cfe37c0778c65fb8b72ff1e537c0c
MerihanYehia <merihan.yehia@fedis.com.eg>
2024-10-26 08:19:57 +0300

    add health checker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  health(): string {
+    return 'App is Up and running';
   }
 }
